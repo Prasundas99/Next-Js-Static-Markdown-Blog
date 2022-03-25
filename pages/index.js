26145debbea21c1b06b0911a916dc7ff27cdbde5
Prasundas99 <prasundas115@gmail.com>
2022-03-25 13:19:34 +0530

    feat:rendered posts

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,52 @@
-import Head from 'next/head'
+import fs from "fs"; /**Not allowed on client side but since we will be using this in getStaticprops or serverside rendering it will allow us */
+import path from "path";
+import matter from "gray-matter";
+import Head from "next/head";
+import Post from "../components/Post";
+import { sortByDate } from "../utils";
 
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div>
       <Head>
-        <title>Blog</title>
+        <title>Blogs</title>
       </Head>
 
-    
+      <div className="posts">
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
     </div>
-  )
+  );
+}
+//(Static Site Generation) from a page, Next.js will pre-render this page at build time using the props returned by getStaticProps.
+export async function getStaticProps() {
+  // Get files from the posts dir
+  const files = fs.readdirSync(path.join("posts"));
+
+  // Get slug and frontmatter from posts
+  const posts = files.map((filename) => {
+    // Create slug
+    const slug = filename.replace(".md", "");
+
+    // Get frontmatter
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
+
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      posts: posts.sort(sortByDate),
+    },
+  };
 }
